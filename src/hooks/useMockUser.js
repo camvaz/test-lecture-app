4e7595aa545f926e9c1c299ad4ed35646b2d1811
camvaz <victor.campos@wizeline.com>
2021-09-29 14:50:25 -0500

    query solution ready

--- src/hooks/useMockUser.js
+++ src/hooks/useMockUser.js
@@ -1,4 +1,12 @@
 import axios from "axios";
+import { useCallback } from "react";
 import { useQuery } from "./useQuery";
 
-export const useMockUser = () => useQuery(() => axios.get());
+export const useMockUser = () => {
+  const memoizedRequest = useCallback(
+    () => axios.get("https://jsonplaceholder.typicode.com/users"),
+    []
+  );
+
+  return useQuery(memoizedRequest);
+};
